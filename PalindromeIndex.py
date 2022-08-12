5a82d6fce721ef57f147500ea38f3e5ca8881a98
minhtutruong712@gmail.com <minhtutruong712@gmail.com>
2022-08-12 15:13:06 +0700

    100% all test case

--- PalindromeIndex.py
+++ PalindromeIndex.py
@@ -1,25 +1,24 @@
 def palindromeIndex(s):
     def isPalindrome(s): 
-        print(1)
         return s == s[::-1]
         
-    def Solve(s): 
+    def Solve (s): 
         i = 0
         j = len(s)
-        while i < j: 
-            if isPalindrome (s[i:j]): 
-                print (i,j)
-                return -1
-            if isPalindrome (s[i+1:j]):
-                print(i,j)
-                return i
-            if isPalindrome (s[i:j-1]):
-                print(i,j)
-                return j-1
+        while i<j and s[i] == s[j-1]:
             i += 1
             j -= 1
+        if i>=j:
+            return -1
+        else: 
+            if isPalindrome(s[i+1:j]): 
+                return i
+            if isPalindrome(s[i:j-1]):
+                return j-1
+        return -1
     return Solve(s)
 
-i = palindromeIndex('bcdefghijklmnopqrstuvwxyza')
-print(i)
+    
+            
+
 
